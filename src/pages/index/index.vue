d50d6ab13e1593a49d2ed0e4d350ec42d7a8e201
hom <homjson233@gmail.com>
2024-04-30 14:46:42 +0800

    first commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <view class="content">
+        <wd-select-picker type="radio" label="基本用法" v-model="model.test" :columns="columns"></wd-select-picker>
+    </view>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const model = ref({
+    test: '',
+})
+const columns = ref([
+    {
+        value: '101',
+        label: '男装',
+    },
+    {
+        value: '102',
+        label: '奢侈品',
+    },
+    {
+        value: '103',
+        label: '女装',
+    },
+])
+</script>
+
+<style>
+.content {
+    display: flex;
+    flex-direction: column;
+}
+</style>
